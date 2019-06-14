acbf78219d23441a119f58ab4ae986c42f227f15
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2019-06-14 01:31:06 -0300

    chore: add and configure poi to build files

---
+++ poi.config.js
@@ -0,0 +1,30 @@
+const {name, version} = require('./package.json')
+
+const demo = process.env.NODE_ENV === 'development' || process.env.npm_lifecycle_event === 'docs:build'
+
+// converts MyComponent to my-component
+const kebabCase = (s) => {
+    return s.replace(/([A-Z])([^A-Z\-])/g, (_, a, b) => `-${a}${b}`)
+        .toLowerCase()
+        .replace(/[\s_-]+/g, '-')
+        .replace(/(^\W)|(\W$)/g, '')
+}
+
+module.exports = {
+    entry: 'src/index.js',
+    babel: {
+        jsx: 'vue'
+    },
+    output: {
+        html: demo,
+        fileNames: {
+            js: demo ? undefined : kebabCase(name) + '.js',
+            css: demo ? undefined : 'style.css',
+            font: demo ? undefined : `assets/fonts/${kebabCase(name)}.[ext]`,
+            image: demo ? undefined : `assets/images/${kebabCase(name)}.[ext]`,
+        },
+    },
+    envs: {
+        VERSION: JSON.stringify(version)
+    }
+}
