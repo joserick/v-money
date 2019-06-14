87f9f399874cd045ce13d1b4533e954f80ceca4d
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2019-06-14 02:56:18 -0300

    fix: fix poi config

--- poi.config.js
+++ poi.config.js
@@ -1,28 +1,14 @@
 const {name, version} = require('./package.json')
 
-const demo = process.env.NODE_ENV === 'development' || process.env.npm_lifecycle_event === 'docs:build'
-
-// converts MyComponent to my-component
-const kebabCase = (s) => {
-    return s.replace(/([A-Z])([^A-Z\-])/g, (_, a, b) => `-${a}${b}`)
-        .toLowerCase()
-        .replace(/[\s_-]+/g, '-')
-        .replace(/(^\W)|(\W$)/g, '')
-}
-
 module.exports = {
-    entry: demo ? 'src/docs/docs.vue' : 'src/index.js',
-    babel: {
-        jsx: 'vue'
-    },
+    entry: 'src/index.js',
     output: {
-        html: demo,
+        html: false,
+        sourceMap: false,
         fileNames: {
-            js: demo ? undefined : kebabCase(name) + '.js',
-            css: demo ? undefined : 'style.css',
-            font: demo ? undefined : `assets/fonts/${kebabCase(name)}.[ext]`,
-            image: demo ? undefined : `assets/images/${kebabCase(name)}.[ext]`,
+            js: name + '.js'
         },
+        format: 'cjs'
     },
     envs: {
         VERSION: JSON.stringify(version)
